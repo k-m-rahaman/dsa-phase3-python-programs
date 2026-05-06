3a58f940c5be529623d84528088b1f3b2003962c
k-m-rahaman <kmrofficial786@gmail.com>
2026-05-06 05:41:40 +0530

    add fibonacci using memoization in python

---
+++ Dynamic_Programming/fibonacci_memo.py
@@ -0,0 +1,14 @@
+def fib(n, dp):
+    if n <= 1:
+        return n
+
+    if dp[n] != -1:
+        return dp[n]
+
+    dp[n] = fib(n-1, dp) + fib(n-2, dp)
+    return dp[n]
+
+
+n = 10
+dp = [-1] * (n + 1)
+print(fib(n, dp))
