2da923d304aa6402271467f51bde3769f8279072
k-m-rahaman <kmrofficial786@gmail.com>
2026-05-07 05:40:31 +0530

    add longest increasing subsequence (LIS)

---
+++ Dynamic_Programming/lis.py
@@ -0,0 +1,13 @@
+def lis(arr):
+    n = len(arr)
+    dp = [1] * n
+
+    for i in range(1, n):
+        for j in range(i):
+            if arr[i] > arr[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+
+print(lis([10,9,2,5,3,7,101,18]))
